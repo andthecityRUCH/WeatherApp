d063b8e32239aa03c581f86e7da441c9e3c07664
Ruchira <ruchira5369@gmail.com>
2025-06-15 03:28:52 -0700

    Create weather.py

---
+++ weather.py
@@ -0,0 +1,19 @@
+import requests
+
+api_key = 'ed24e03df58d946443df3ddad4bd180e'
+
+city = input('Enter city name: ')
+
+url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
+    temp = data['main']['temp']
+    desc = data['weather'][0]['description']
+    print(f'Temperature: {temp} K')
+    print(f'Description: {desc}')
+else:
+    print('Error fetching weather data')
+
